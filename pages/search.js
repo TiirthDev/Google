e3884049a64cdcf374935d81e254235ff967f0b7
Tiirth <tiirthdeora@gmail.com>
2021-09-25 19:44:42 +0530

    final

--- pages/search.js
+++ pages/search.js
@@ -32,12 +32,11 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useDummyData = false;
-  const startIndex = context.query.start || "0";
 
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
       ).then((response) => response.json());
 
   return {
